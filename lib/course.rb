f64ae276bcbbc4f20e64a671db6bcd560e430d0a
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-19 09:09:42 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,11 +1,6 @@
 class Course 
   attr_accessor :title, :schedule, :description
   
-  def initialize(title, schedule, description)
-    @title = title 
-    @schedule = schedule
-    @description = description
-  end 
   
 end 
 
